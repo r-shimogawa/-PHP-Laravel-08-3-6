d6b7b3a9918de1f2bdeb266b23ba553a43c9e1c5
r-shimogawa <shimogawa1992@gmail.com>
2022-10-30 02:06:00 +0000

    first commit

--- web.php
+++ web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 
@@ -31,9 +31,13 @@ Route::controller(AAAController::class)->group(function() {
 });
 
 
-// 【PHP/Laravel】04　4
+// 【PHP/Laravel】04　4　+　【PHP/Laravel】07 2~3
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(\ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
